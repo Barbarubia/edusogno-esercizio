b82fdee69f1f446183be8334b6cdd2b3b632d1f7
Barbarubia <dnl.desimone@gmail.com>
2022-11-10 09:57:31 +0100

    creato file per pagina utente

---
+++ user.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edusogno - Pagina Utente</title>
+</head>
+
+<body>
+
+    <header>
+        <!-- Qui va l'header con il logo -->
+    </header>
+
+    <main>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident corporis voluptate beatae eligendi quae veritatis, dolor possimus. Quisquam ea est sunt aperiam fugit deserunt, quis, quas corporis dolorum assumenda id!
+    </main>
+    
+</body>
+
+</html>
